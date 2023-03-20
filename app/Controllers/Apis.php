72c9365e837b14022b83969827d4c811d877af7b
Loucas Xiourouppa <2007307@csl-student.wlv.ac.uk>
2023-03-20 22:02:49 +0000

    Succesfully corrected api to work with a view now!

--- app/Controllers/Apis.php
+++ app/Controllers/Apis.php
@@ -40,45 +40,12 @@ class Apis extends BaseController
             $data = json_decode($response, true);
 
             // Pass parsed data to view
-            echo view('templates/header', $data);
-			
-            echo view('apis/videogamenews', $data);
-            echo view('templates/footer', $data);
-			
-			// Use HTML markup to structure the output
-echo '<div class="blog-posts">';
-foreach ($data as $item) {
-    echo '<div class="blog-post">';
-    echo '<h2>' . $item['title'] . '</h2>';
-    echo '<p class="date">' . $item['date'] . '</p>';
-    echo '<img src="' . $item['image'] . '" alt="' . $item['title'] . '">';
-    echo '<p class="description">' . $item['description'] . '</p>';
-    echo '<a href="' . $item['link'] . '">Read more</a>';
-    echo '</div>';
-}
-echo '</div>';
-
-// Add some CSS styling
-echo '<style>';
-echo '.blog-posts { display: grid; grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); grid-gap: 20px; }';
-echo '.blog-post { padding: 20px; background-color: #fff; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }';
-echo '.blog-post h2 { margin-top: 0; }';
-echo '.blog-post .date { font-size: 14px; color: #999; margin-bottom: 10px; }';
-echo '.blog-post img { max-width: 100%; height: auto; margin-bottom: 10px; }';
-echo '.blog-post .description { margin-bottom: 10px; }';
-echo '.blog-post a { display: inline-block; padding: 5px 10px; background-color: #007bff; color: #fff; border-radius: 5px; text-decoration: none; }';
-echo '.blog-post a:hover { background-color: #0069d9; }';
-echo '</style>';
-
-// Add some error handling
-if ($err) {
-    if (curl_errno($curl) == 28) {
-        echo '<p>Sorry, the server is currently busy. Please try again later.</p>';
-    } else {
-        echo '<p>There was an error loading the blog posts. Please try again later.</p>';
-    }
+			echo view('templates/header', $data);
+			echo view('apis/videogamenews', ['data' => $data]);
+			echo view('templates/footer', $data);
 
 		}
-				}
     }
 }
+
+
--- app/Views/apis/videogamenews.php
+++ app/Views/apis/videogamenews.php
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html>
 <h2>Recent Articles</h2>
+<head>
+	<title>Video Game News</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<style>
+		.card {
+			margin-bottom: 30px;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<?php foreach ($data as $item) { ?>
+			<div class="col-md-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= $item['image'] ?>" alt="<?= $item['title'] ?>">
+					<div class="card-body">
+						<h5 class="card-title"><?= $item['title'] ?></h5>
+						<p class="card-text"><?= $item['description'] ?></p>
+						<a href="<?= $item['link'] ?>" class="btn btn-primary">Read More</a>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
+
