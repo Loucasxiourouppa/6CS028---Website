ae7e3ea0a80a177deba7f6eb76585e189c0f4fb5
Loucas Xiourouppa <2007307@csl-student.wlv.ac.uk>
2023-03-20 17:04:13 +0000

    Changed text in About page

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,4 +1,3 @@
-<!-- templates/header.php -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,22 +11,22 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h1>About Us</h1>
-			<p>Welcome to our gaming blog, where we share our thoughts and reviews on the latest video games. Our goal is to provide valuable insights and opinions to the gaming community, so that you can make informed decisions about the games you play.</p>
-
+			<p>Welcome to GameSphere, where you can find the latest news on games and leave a review. The goal is to build a community and maybe even play games together!.</p>
 			<h2>Meet the Team</h2>
-			<p>We are a team of passionate gamers who love nothing more than spending hours immersed in a great video game. Here are some brief introductions:</p>
-
 			<ul>
-				<li><strong>Loucas Xiourouppa</strong> - Loucas is a student at the university of Wolverhampton Styding Computer Science.</li>
+				<li><strong>Loucas Xiourouppa</strong> - Loucas is a student at the university of Wolverhampton Styding Computer Science. Hobbies are playing games and watching Formula 1. My Favourite video game at the moment is Formula 1 2022. </li>
 				
 			</ul>
-
-			<p>If you have any questions or comments, please don't hesitate to email us at gamesphere@gmail.com <p>
+				<h2>Goals</h2>
+				<p>The goal of the website is to build a community that is nice to each other and welcomes newcomers! <p>
+				<p>We ask to please be considerate and respectfull to each other. Do not share any personal information unless you know the user. <p>
+				
+			<p>If you have any questions or comments, please don't hesitate to email us at gamesphere@gmail.com<p>
 		</div>
 	</div>
 </div>
 
-<!-- templates/footer.php -->
+
 <footer>
 	<p>&copy; 2023 Gaming Blog. All rights reserved.</p>
 </footer>
